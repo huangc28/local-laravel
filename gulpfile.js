5680164e42a8abf876a4b9257698ee89d1180087
紀衡 黃 <huangchiheng@gmail.com>
2015-10-12 18:04:29 +0800

    WW-1439 add less and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var path = require('path');
 // console.log(path.join(__dirname, 'less', 'includes'));
 
 /**
- *
+ * 1. Include main less file
+ * 2. Search path for 
  */
 gulp.task('less', function(){
 	return gulp.src('./public/less/*.less')
