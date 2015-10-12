346efb2e006f53a3c681486251c4039f5f454e27
紀衡 黃 <huangchiheng@gmail.com>
2015-10-12 12:16:33 +0800

    hello

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
+// console.log(path.join(__dirname, 'less', 'includes'));
+
+/**
+ *
  */
-
-elixir(function(mix) {
-    mix.sass('app.scss');
+gulp.task('less', function(){
+	return gulp.src('./public/less/*.less')
+		.pipe(less({
+			paths: [ path.join(__dirname, 'less', 'includes') ]
+		}))
+		.pipe(gulp.dest('./public/css'));
 });
